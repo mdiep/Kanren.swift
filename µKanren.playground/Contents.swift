823a4c543d7205b7898e73a7c5c4225c2befe094
Matt Diephouse <matt@diephouse.com>
2016-07-25 21:36:49 -0400

    Add some documentation

--- µKanren.playground/Contents.swift
+++ µKanren.playground/Contents.swift
@@ -1,6 +1,9 @@
 
+/// An unknown value.
 struct Variable: CustomStringConvertible, Hashable {
+    /// A number determines the variable's identity.
     let index: UInt
+    
     var description: String { return "#\(index)" }
     var hashValue: Int { return Int(index) }
 }
@@ -9,10 +12,15 @@ func ==(lhs: Variable, rhs: Variable) -> Bool {
     return lhs.index == rhs.index
 }
 
+/// An item that can be compared with µKanren.
 indirect enum Term<Value: Equatable>: CustomStringConvertible, Equatable {
+    /// An known value.
     case variable(Variable)
+    /// A concrete value.
     case value(Value)
+    /// A pair of terms, which can be used to construct larger values.
     case pair(Term<Value>, Term<Value>)
+    /// An empty term, used to terminate lists made of terms.
     case empty
     
     var description: String {
@@ -24,6 +32,7 @@ indirect enum Term<Value: Equatable>: CustomStringConvertible, Equatable {
         }
     }
     
+    /// Construct a term representing a list of terms.
     init<C: Collection where C.Iterator.Element == Term<Value>>(_ collection: C) {
         var iterator = collection.reversed().makeIterator()
         var term = Term.empty
@@ -35,6 +44,7 @@ indirect enum Term<Value: Equatable>: CustomStringConvertible, Equatable {
 }
 
 extension Term: ArrayLiteralConvertible {
+    /// Easily create a list term from an array literal.
     init(arrayLiteral elements: Term<Value>...) {
         self = .init(elements)
     }
@@ -55,12 +65,19 @@ func == <Value>(lhs: Term<Value>, rhs: Term<Value>) -> Bool {
     }
 }
 
+/// Error used with `State.unify()` fails to unify.
 struct UnificationError: ErrorProtocol { }
 
+/// A state is a set of assocations and a counter representing the next variable.
 struct State<Value: Equatable>: CustomStringConvertible {
+    /// A goal--something that takes an initial state and returns an iterator
+    /// of all the states that can be achieved by meeting that goal.
     typealias Goal = (State<Value>) -> AnyIterator<State<Value>>
     
+    /// Known assocations between variables and other terms (e.g., x=5 or x=y).
     var substitution: [Variable: Term<Value>] = [:]
+    
+    /// The index of the next unused variable.
     var counter: UInt = 0
     
     var variables: [Variable] {
@@ -80,14 +97,16 @@ struct State<Value: Equatable>: CustomStringConvertible {
             .joined(separator: ", ")
     }
     
-    /// ext-s
+    /// `ext-s` in µKanren
+    /// Extend the state by adding another variable -> term assocation.
     func extend(_ variable: Variable, _ term: Term<Value>) -> State {
         var state = self
         state.substitution[variable] = term
         return state
     }
     
-    /// walk
+    /// `walk` in µKanren
+    /// Find the value of a term in the state, which may be the term itself.
     func walk(_ term: Term<Value>) -> Term<Value> {
         if case let .variable(variable) = term, let value = substitution[variable] {
             return walk(value)
@@ -98,7 +117,9 @@ struct State<Value: Equatable>: CustomStringConvertible {
         }
     }
     
-    /// unify
+    /// `unify` in µKanren
+    /// Attempt to unify two terms in the state--i.e., make them equivalent. If
+    /// the terms can't be unified, throws a UnificationError.
     func unify(_ a: Term<Value>, _ b: Term<Value>) throws -> State<Value> {
         let a = walk(a)
         let b = walk(b)
@@ -118,6 +139,7 @@ struct State<Value: Equatable>: CustomStringConvertible {
     }
 }
 
+/// A new goal that tries to make two terms equal.
 func == <Value>(lhs: Term<Value>, rhs: Term<Value>) -> State<Value>.Goal {
     return { state in
         do {
@@ -128,22 +150,29 @@ func == <Value>(lhs: Term<Value>, rhs: Term<Value>) -> State<Value>.Goal {
     }
 }
 
+/// Overload to make comparing variables easier.
 func == <Value>(lhs: Variable, rhs: Variable) -> State<Value>.Goal {
     return .variable(lhs) == .variable(rhs)
 }
 
+/// Overload to make comparing a variable and a value easier.
 func == <Value>(lhs: Variable, rhs: Value) -> State<Value>.Goal {
     return .variable(lhs) == .value(rhs)
 }
 
+/// `disj` in µKanren
+/// A goal that succeeds when either goal succeeds.
 func || <Value>(lhs: State<Value>.Goal, rhs: State<Value>.Goal) -> State<Value>.Goal {
     return { state in lhs(state).interleave(with: rhs(state)) }
 }
 
+/// `conj` in µKanren
+/// A goal that succeeds when both goals succeed.
 func && <Value>(lhs: State<Value>.Goal, rhs: State<Value>.Goal) -> State<Value>.Goal {
     return { state in lhs(state).flatMap(rhs) }
 }
 
+/// Introduce a new variable that can be used to create a goal.
 func fresh<Value>(block: (Variable) -> State<Value>.Goal) -> State<Value>.Goal {
     return { state in
         var state = state
@@ -153,13 +182,19 @@ func fresh<Value>(block: (Variable) -> State<Value>.Goal) -> State<Value>.Goal {
     }
 }
 
+/// Introduce two new variables that can be used to create a goal.
 func fresh<Value>(block: (Variable, Variable) -> State<Value>.Goal) -> State<Value>.Goal {
     return fresh { x in fresh { y in { state in block(x, y)(state) } } }
 }
 
+/// Introduce three new variables that can be used to create a goal.
 func fresh<Value>(block: (Variable, Variable, Variable) -> State<Value>.Goal) -> State<Value>.Goal {
     return fresh { x in fresh { y in fresh { z in { state in block(x, y, z)(state) } } } }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// A goal that demonstrates some basic logic.
 
 let goal1 = fresh { a, b in
     a == 7 && (a == b || b == 6)
@@ -169,6 +204,10 @@ iter1.next()
 iter1.next()
 iter1.next()
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// A goal that demonstrates that items in lists can be compared.
+
 let goal2 = fresh { x, y, z in
     [.variable(x), .value(4), .variable(z)] == [.value(1), .variable(y), .value(3)]
 }
@@ -176,6 +215,9 @@ let iter2 = goal2(State())
 iter2.next()
 iter2.next()
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// A goal that tries to combine two lists to equal a third list.
 func append<Value>(_ head: Term<Value>, _ tail: Term<Value>, _ list: Term<Value>) -> State<Value>.Goal {
     return (head == .empty && tail == list)
         || fresh { first, restOfHead, restOfList in
@@ -184,6 +226,8 @@ func append<Value>(_ head: Term<Value>, _ tail: Term<Value>, _ list: Term<Value>
                 && append(.variable(restOfHead), tail, .variable(restOfList))
     }
 }
+
+// A goal that demostrates append.
 
 let goal3 = fresh { x in
     append([.value("h"), .value("e")], [.value("l"), .value("l"), .value("o")], .variable(x))
